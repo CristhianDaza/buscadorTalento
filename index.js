da0e6db7e6ad05c50b1786d4adc7685f432472f3
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-29 18:58:41 -0500

    Creando el router

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const router = require('./routes')
 
 const app = express()
 
-app.use('/', (req, res) => {
-  res.send('Iniciando')
-})
+app.use('/', router())
 
 app.listen(5000)
