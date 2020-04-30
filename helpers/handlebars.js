f77ecdc2d5c4d156197812c7743e0eee23845221
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-30 16:47:15 -0500

    Autollenando el formulario con la información a editar

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -6,10 +6,16 @@ module.exports = {
     skills.forEach(skill => {
       html +=
       `
-        <li>${skill}</li>
+        <li ${seleccionadas.includes(skill) ? ' class="activo"' : ''}> ${skill} </li>
       `
     })
 
     return opciones.fn().html = html
+  },
+
+  tipoContrato: (seleccionado, opciones) => {
+    return opciones.fn(this).replace(
+      new RegExp(`value="${seleccionado}"`), '$& selected="selected"'
+    )
   }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const skills = document.querySelector('.lista-conocimientos')
   if (skills) {
     skills.addEventListener('click', agregarSkills)
+
+    // una vez que estamos en editar, llamar la funcion
+    skillSeleccionados()
   }
 })
 
@@ -22,4 +25,16 @@ const agregarSkills = e => {
   const skillsArray = [...skills]
   document.querySelector('#skills').value = skillsArray
 
+}
+
+const skillSeleccionados = () => {
+  const seleccionados = Array.from(document.querySelectorAll('.lista-conocimientos .activo'))
+
+  seleccionados.forEach(seleccionada => {
+    skills.add(seleccionada.textContent)
+  })
+
+  // inyectarlo en el hidden
+  const skillsArray = [...skills]
+  document.querySelector('#skills').value = skillsArray
 }
