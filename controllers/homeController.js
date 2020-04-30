6b383ca461c39fee301a69fb62bd951363594ab2
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-30 13:10:32 -0500

    Mostrando vacantes en la pagina principal

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,16 @@
-exports.mostrarTrabajos = (req, res) => {
+const mongoose = require('mongoose')
+const Vacante = mongoose.model('Vacante')
+
+exports.mostrarTrabajos = async (req, res, next) => {
+  const vacantes = await Vacante.find().lean()
+
+  if (!vacantes) return next()
+
   res.render('home', {
     nombrePagina: 'devJobs',
     tagline: 'Encuentra y Pública trabajos para Desarrolladores Web',
     barra: true,
-    boton: true
+    boton: true,
+    vacantes
   })
 }
