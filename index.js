b9d76526032f54370f9ee858d5e129c3b0b3c4e1
Cristhian Daza <cristian.d.b@hotmail.com>
2020-04-29 21:46:40 -0500

    Contectando a la base de datos. Se elimino dependencia session por express-session

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const mongoose = require('mongoose')
+require('./config/db')
+
 const express = require('express')
 const exhbs = require('express-handlebars')
 const path = require('path')
 const router = require('./routes')
 require('dotenv').config({ path: './variables.env' })
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const app = express()
 
@@ -17,6 +23,16 @@ app.set('view engine', 'handlebars')
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cookieParser())
+
+app.use(session({
+  secret: process.env.SECRETO,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
 
 app.use('/', router())
 
