3b4ad3f7317eb511ad8e58ec2b630d6ac8f9d311
Cristhian Daza <cristian.d.b@hotmail.com>
2020-05-10 21:09:50 -0500

    Creando el codigo para enviar el email

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Vacante = mongoose.model('Vacante')
 const Usuarios = mongoose.model('Usuarios')
 const crypto = require('crypto')
+const enviarEmail = require('../handlers/email')
 
 exports.autenticarUsaurio = passport.authenticate('local', {
   successRedirect: '/administracion',
@@ -68,7 +69,14 @@ exports.enviarToken = async (req, res) => {
   await usuario.save()
   const resetUrl = `http//${req.headers.host}/reestablecer-password/${usuario.token}`
 
-  // TODO: Enviar notificacion por email
+  // Enviar notificacion por email
+  await enviarEmail.enviar({
+    usuario,
+    subject: 'Password Reset',
+    resetUrl,
+    archivo: 'reset'
+  })
+  // todo correcto
   req.flash('correcto', 'Revisa tu email para las indicaciones')
   res.redirect('/iniciar-sesion')
 }
--- handlers/email.js
+++ handlers/email.js
@@ -11,3 +11,18 @@ let transport = nodemailer.createTransport({
     pass: emailConfig.pass
   }
 })
+
+exports.enviar = async (opciones) => {
+  const opcionesEmail = {
+    from: 'devJobs <noreply@devjobs.com',
+    to: opciones.usuario.email,
+    subject: opciones.subject,
+    template: opciones.archivo,
+    context: {
+      resetUrl: opciones.resetUrl
+    }
+  }
+
+  const sendMail = util.promisify(transport.sendMail, transport)
+  return sendMail.call(transport, opcionesEmail)
+}
